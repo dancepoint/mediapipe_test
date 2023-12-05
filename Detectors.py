ea7b1ec0b1b02120b4667b06920c5ed98628fddf
Ibrahima Capo-Chichi <superblastpower@gmail.com>
2023-12-05 12:44:41 -0500

    initial

---
+++ Detectors.py
@@ -0,0 +1,38 @@
+
+import cv2
+import mediapipe as mp
+import numpy as np
+import PoseModule as pm
+
+
+
+cap = cv2.VideoCapture(0)
+detector = pm.poseDetector()
+count = 0
+direction = 0
+form = 0
+feedback = "Fix Form"
+
+
+while cap.isOpened():
+    ret, img = cap.read() #640 x 480
+    #Determine dimensions of video - Help with creation of box in Line 43
+    width  = cap.get(3)  # float `width`
+    height = cap.get(4)  # float `height`
+    # print(width, height)
+    
+    img = detector.findPose(img, False)
+    lmList = detector.findPosition(img, False)
+    # print(lmList)
+    if len(lmList) != 0:
+        elbow = detector.findAngle(img, 11, 13, 15)
+        shoulder = detector.findAngle(img, 13, 11, 23)
+        hip = detector.findAngle(img, 11, 23,25)
+        
+
+    cv2.imshow('Rep counter', img)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+        
+cap.release()
+cv2.destroyAllWindows()
